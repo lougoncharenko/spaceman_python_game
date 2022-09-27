201d6fc2ae0fda267397dcc486ed5204646c575e
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-09-27 18:33:15 -0400

    added letters_guessed array

--- main.py
+++ main.py
@@ -28,17 +28,19 @@ def choose_secret_word():
 #     #TODO: Loop through the letters in secret word and build a string that shows the letters that 
 #     # have been guessed correctly so far that are saved in letters_guessed and underscores for 
 #     # the letters that have not been guessed yet
+def get_guessed_word(secret_word, letters_guessed):
+    print(letters_guessed)
+    print(secret_word)
+
 
 
 # #--------------------------------------------------------------------------------
 # # A function to check if the guessed letter is in the secret word
-# def is_guess_in_word(guess, secret_word):
-#     #TODO: check if the letter guess is in the secret word
 def is_guess_in_word(guess, secret_word):
-        if guess in secret_word:
-            print("It's a match")
-        else:
-            print('No match')
+    if guess in secret_word:
+        print("It's a match")
+    else:
+        print('No match')
 
 
 
@@ -47,9 +49,12 @@ def is_guess_in_word(guess, secret_word):
 def play_spaceman(secret_word):
     print(secret_word)
     guess = 0
+    letters_guessed= []
     while guess < 7:
         userInput = input('Enter a letter to guess: ')
         is_guess_in_word(userInput, secret_word)
+        letters_guessed.append(userInput)
+        get_guessed_word(secret_word, letters_guessed)
         guess += 1
 
 
