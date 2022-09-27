4d8301f00db6b12f1dd127e72f6550746f881e74
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-09-27 18:10:26 -0400

    added if/else statement in is_guess_in_word

--- main.py
+++ main.py
@@ -17,22 +17,29 @@ def choose_secret_word():
 #--------------------------------------------------------------------------------
 # #    A function that checks if all the letters of the secret word have been guessed.
 # def is_word_guessed(secret_word, letters_guessed):
-#      # TODO: Loop through the letters in the secret_word and check if a letter is not in lettersGuessed
-
-
-
+     # TODO: Loop through the letters in the secret_word and check if a letter is not in lettersGuessed
+# 
 # #--------------------------------------------------------------------------------
 # #A function that is used to get a string showing the letters guessed so far in the secret word and underscores 
 # # for letters that have not been guessed yet.
 # def get_guessed_word(secret_word, letters_guessed):
+#      for letter in secret_word:
+#         print(letter)
 #     #TODO: Loop through the letters in secret word and build a string that shows the letters that 
 #     # have been guessed correctly so far that are saved in letters_guessed and underscores for 
 #     # the letters that have not been guessed yet
+
 
 # #--------------------------------------------------------------------------------
 # # A function to check if the guessed letter is in the secret word
 # def is_guess_in_word(guess, secret_word):
 #     #TODO: check if the letter guess is in the secret word
+def is_guess_in_word(guess, secret_word):
+        if guess in secret_word:
+            print("It's a match")
+        else:
+            print('No match')
+
 
 
 # #--------------------------------------------------------------------------------
@@ -42,9 +49,10 @@ def play_spaceman(secret_word):
     guess = 0
     while guess < 7:
         userInput = input('Enter a letter to guess: ')
+        is_guess_in_word(userInput, secret_word)
         guess += 1
 
 
 #These function calls that will start the game
-secret_word= choose_secret_word()   
+secret_word = choose_secret_word()  
 play_spaceman(secret_word)
